28487bd307fbda8cdb5b0610787a2c7d83608d65
s1520217 <nagatomo1218@gmail.com>
2015-06-17 18:36:48 +0900

    URL-site_name converter

--- app/src/main/java/com/example/nagatomo/rssreader/ItemDetailJapanActivity.java
+++ app/src/main/java/com/example/nagatomo/rssreader/ItemDetailJapanActivity.java
@@ -1,6 +1,7 @@
 package com.example.nagatomo.rssreader;
 
 import android.app.Activity;
+import android.os.StrictMode;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.content.Intent;
@@ -13,6 +14,12 @@ import android.net.Uri;
 import android.view.View;
 import android.app.AlertDialog;
 import android.view.View.OnClickListener;
+import android.text.util.Linkify;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ItemDetailJapanActivity extends Activity {
     private TextView mTitle;
@@ -37,6 +44,77 @@ public class ItemDetailJapanActivity extends Activity {
         mTitle.setTextColor(Color.parseColor("magenta"));
 
 
+
+//特定のURLに対して、そのtitle、すなわちサイト名のみを抜き出す
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+
+        HttpURLConnection http = null;
+        InputStream in = null;
+        TextView web = (TextView) findViewById(R.id.textViewURL);
+        try {
+            // URLにHTTP接続
+            String s = "https://www.taylorguitars.com/";
+            URL url = new URL(String.valueOf(s));
+            http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("GET");
+            http.connect();
+            // データを取得
+            in = http.getInputStream();
+
+            String src = new String();
+            byte[] line = new byte[1024];
+            int size;
+            while (true) {
+                size = in.read(line);
+                if (size <= 0)
+                    break;
+                src += new String(line);
+            }
+            // HTMLソースを表示
+            int index = src.indexOf("<title>");
+            String src2 =  src.substring(index + 7);
+            int index2 = src2.indexOf("</title>");
+            String src3 =  src2.substring(0, index2);
+            //String trans = new String(String.valueOf(src3).getBytes("EUC_JP"), "EUC_JP");
+            web.setText(src3);
+        } catch (Exception e) {
+            web.setText(e.toString());
+        } finally {
+            try {
+                if (http != null)
+                    http.disconnect();
+                if (in != null)
+                    in.close();
+            } catch (Exception e) {
+            }
+        }
+
+
+
+
+        //特定の文字列にリンクを付ける
+        TextView text = (TextView)findViewById(R.id.linktext1);
+        text.setText("TechboosterではAndroidを中心に技術解説しています。Techboosterはほぼ毎日更新しています。");
+        Pattern pattern = Pattern.compile("Techbooster");
+        final String strUrl = "http://www.techbooster.org/";
+
+        Linkify.TransformFilter filter = new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher match, String url) {
+                return strUrl;
+            }
+        };
+
+        Linkify.addLinks(text, pattern, strUrl, null, filter);
+
+
+
+
+
+
+
+
+//以下、webViewに飛ぶようにするコード
         HandleableLinkMovementMethod linkMethod = new HandleableLinkMovementMethod();
         linkMethod.setOnUrlClickListener(new OnUrlClickListener() {
             @Override
@@ -54,5 +132,12 @@ public class ItemDetailJapanActivity extends Activity {
 /*      MovementMethod mMethod = LinkMovementMethod.getInstance();
         mDescr.setMovementMethod(mMethod);
         mDescr.setText(cs2);*/
+
+
+
+
+
+
+
     }
 }
