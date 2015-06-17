18bc81e8b975c7a5c5b6296b7d7f59019fcf7787
s1520217 <nagatomo1218@gmail.com>
2015-06-17 14:17:15 +0900

    add JapanNews

---
+++ app/src/main/java/com/example/nagatomo/rssreader/ItemDetailJapanActivity.java
@@ -0,0 +1,58 @@
+package com.example.nagatomo.rssreader;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.content.Intent;
+import android.text.Html;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Color;
+import com.example.nagatomo.rssreader.HandleableLinkMovementMethod.OnUrlClickListener;
+import android.net.Uri;
+import android.view.View;
+import android.app.AlertDialog;
+import android.view.View.OnClickListener;
+
+public class ItemDetailJapanActivity extends Activity {
+    private TextView mTitle;
+    private TextView mDescr;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itemdetail);
+
+        //受け取り側のActivityは設定されてるkey(ここではTITLE)で値を取り出す
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("TITLE");
+        String descr = intent.getStringExtra("DESCRIPTION");
+        CharSequence cs1 = Html.fromHtml(title);
+        CharSequence cs2 = Html.fromHtml(descr);
+        mTitle = (TextView) findViewById(R.id.item_detail_title);//findViewByIdでリソースIDに対応するビューのオブジェクトを取得する
+        // mTitle.setText(title);  //変数titleを、mTitleオブジェクトのsetTextメソッドに渡して何かしらの結果を返す
+        mDescr = (TextView) findViewById(R.id.item_detail_descr);
+        //  mDescr.setText(descr);
+        mTitle.setText(cs1);
+        mTitle.setTextColor(Color.parseColor("magenta"));
+
+
+        HandleableLinkMovementMethod linkMethod = new HandleableLinkMovementMethod();
+        linkMethod.setOnUrlClickListener(new OnUrlClickListener() {
+            @Override
+            public void onUrlClick(Uri uri) {
+                // ここでuriを使ってWebView表示用のIntentを飛ばしたりする
+                WebView webView = (WebView)findViewById(R.id.webview);
+                webView.setWebViewClient(new WebViewClient());
+                webView.loadUrl(String.valueOf(uri));
+                mDescr.setVisibility(View.GONE);
+                //  Layout.removeView(mDescr);
+            }
+        });
+        mDescr.setMovementMethod(linkMethod);
+        mDescr.setText(cs2);
+/*      MovementMethod mMethod = LinkMovementMethod.getInstance();
+        mDescr.setMovementMethod(mMethod);
+        mDescr.setText(cs2);*/
+    }
+}
