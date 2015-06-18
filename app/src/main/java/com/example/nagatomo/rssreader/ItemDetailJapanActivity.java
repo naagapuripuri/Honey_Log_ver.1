125ef1144c3a5cb59f776ab6b795f6c8be2117e3
s1520217 <nagatomo1218@gmail.com>
2015-06-18 23:39:38 +0900

    can get relative news automatically(many patterns)

--- app/src/main/java/com/example/nagatomo/rssreader/ItemDetailJapanActivity.java
+++ app/src/main/java/com/example/nagatomo/rssreader/ItemDetailJapanActivity.java
@@ -29,7 +29,11 @@ public class ItemDetailJapanActivity extends Activity {
     private TextView mURL3;
     private TextView mURL4;
     private String src3;
+    private String src6;
     private String srcurl3;
+    private String srcurl6;
+    private String titlecut;
+    private CharSequence cs1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,8 +43,9 @@ public class ItemDetailJapanActivity extends Activity {
         Intent intent = getIntent();
         String title = intent.getStringExtra("TITLE");
         String descr = intent.getStringExtra("DESCRIPTION");
-
-        CharSequence cs1 = Html.fromHtml(title);
+        titlecut = title.substring(0, 5);
+     //   CharSequence cs1 = Html.fromHtml(title);
+        cs1 = Html.fromHtml(titlecut);
         CharSequence cs2 = Html.fromHtml(descr);
         mTitle = (TextView) findViewById(R.id.item_detail_title);//findViewByIdでリソースIDに対応するビューのオブジェクトを取得する
         // mTitle.setText(title);  //変数titleを、mTitleオブジェクトのsetTextメソッドに渡して何かしらの結果を返す
@@ -63,8 +68,9 @@ public class ItemDetailJapanActivity extends Activity {
         try {
             // URLにHTTP接続
             String s01 = "https://news.google.com/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss&q=";
-            String s02 = "kagoshima";
-            String s = s01 + s02;
+            String s02 = "biyougeka";
+            String s03 = cs1.toString();
+            String s = s01 + s03;
             URL url = new URL(String.valueOf(s));
             http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod("GET");
@@ -102,9 +108,18 @@ public class ItemDetailJapanActivity extends Activity {
             int index4 = src4.indexOf("<item><title>");
             String src5 =  src4.substring(index4 + 13);
             int index5 = src5.indexOf("</title>");
-            String src6 =  src5.substring(0, index5);
+            src6 =  src5.substring(0, index5);
             //String trans = new String(String.valueOf(src3).getBytes("EUC_JP"), "EUC_JP");
             web3.setText(src6);
+
+            int indexurl3 = src.indexOf(String.valueOf(srcurl3));
+            String srcurl4 =  src.substring(indexurl3 + 9);
+            int indexurl4 = srcurl4.indexOf("&amp;url=");
+            String srcurl5 =  srcurl4.substring(indexurl4 + 9);
+            int indexurl5 = srcurl5.indexOf("</link>");
+            srcurl6 =  srcurl5.substring(0, indexurl5);
+            //String trans = new String(String.valueOf(src3).getBytes("EUC_JP"), "EUC_JP");
+            web2.setText(srcurl6);
 
     /*        int index6 = src.indexOf(String.valueOf(src6));
             String src7 =  src.substring(index6 + 13);
@@ -148,6 +163,19 @@ public class ItemDetailJapanActivity extends Activity {
         //以下、繰り返し
         String murl3 = intent.getStringExtra("URL3");
         mURL3 = (TextView) findViewById(R.id.item_detail_url3);
+        mURL3.setText(src6);
+        Pattern pattern3 = Pattern.compile(src6);
+        final String strUrl3 = srcurl6 ;
+        Linkify.TransformFilter filter3 = new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher match, String url) {
+                return strUrl3;
+            }
+        };
+        Linkify.addLinks(mURL3, pattern3, strUrl3, null, filter3);
+
+     /*   String murl3 = intent.getStringExtra("URL3");
+        mURL3 = (TextView) findViewById(R.id.item_detail_url3);
         mURL3.setText(murl3);
         Pattern pattern3 = Pattern.compile(murl3);
         final String strUrl3 = "http://www.techbooster.org/" ;
@@ -158,7 +186,7 @@ public class ItemDetailJapanActivity extends Activity {
             }
         };
         Linkify.addLinks(mURL3, pattern3, strUrl3, null, filter3);
-
+*/
         final String strUrl4 = intent.getStringExtra("URL4");
         String murl4 = "techbooster";
         mURL4 = (TextView) findViewById(R.id.item_detail_url4);
